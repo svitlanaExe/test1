0dc99c79abd9d9758ae40ea5551848c19b1d6559
Svitlana Chaplinska <svitlana@Svitlanas-MacBook-Pro.local>
2020-05-01 23:24:44 +0300

    Task 5 done comments add

--- task5.js
+++ task5.js
@@ -6,7 +6,9 @@ getRandomHexColor(); // #e99064
 getRandomHexColor(); // #7c0724
 
 function getRandomHexColor(arr) {
-let rand = Math.floor(Math.random()*1000000);
+    //generating a random number
+    let rand = Math.floor(Math.random()*1000000);
+    //converting to the to 16-bit number and adding zeros if needed
     rand =  (Math.abs(rand).toString(16)).padStart(6, '0');
     return console.log('#'+rand);
 }
