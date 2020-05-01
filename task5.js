67bb57f96964fa674590a3e8a98d6168046f0c9a
Svitlana Chaplinska <svitlana@Svitlanas-MacBook-Pro.local>
2020-05-01 23:30:35 +0300

    Task 5 upd

--- task5.js
+++ task5.js
@@ -9,12 +9,8 @@ function getRandomHexColor(arr) {
     //generating a random number
     let rand = Math.floor(Math.random()*1000000);
     //converting to the to 16-bit number and adding zeros if needed
-    rand =  (Math.abs(rand).toString(16)).padStart(6, '0');
+    rand =  (Math.abs(rand).toString(16))
+            .padStart(6, '0');
     return console.log('#'+rand);
 }
 
-
-// Math.floor, Math.random (try to generate 2^24 random number)
-// Number.prototype.toString() (try to convert random number to 16-bit number)
-// Array.map (iterate the original "arr" array )
-// String.prototype.padStart (to add some zeros at the beginning of the string, e.g #f724 -> #00f724, #7d2d8 -> #07d2d8 )
