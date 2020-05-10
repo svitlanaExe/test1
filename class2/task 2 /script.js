b7e17774637b0392c5ed4bbd488a71ee10312228
Svitlana Chaplinska <svitlana@Svitlanas-MacBook-Pro.local>
2020-05-10 12:04:49 +0300

    Update

--- class2/task 2 /script.js
+++ class2/task 2 /script.js
@@ -3,14 +3,32 @@ function jQueryMini(element) {
 
     // Use your imagination below :)
     return {
-        css: /* ...some magic... */,
-        addClass: /* ...some magic... */,
-        removeClass: /* ...some magic... */,
-        html: /* ...some magic... */,
+        css: function (property, value){
+            initializedElement.style[property]= value;
+            return this;
+        },
+        addClass: function(className){
+        initializedElement.classList.add(className);
+        return this;
+        }/* ...some magic... */,
+        removeClass: function(className){
+            initializedElement.classList.remove(className);
+            return this;
+        } /* ...some magic... */,
+        html: function(data){
+            if (data){
+                initializedElement.innerHTML = data;
+                return this;
+            }
+            return initializedElement.outerHTML;
+        }
+     /* ...some magic... */,
 
     }
 }
+/*
 
+ */
 
 window.onload = function() { // will code after page loaded
     // Usage
@@ -24,8 +42,9 @@ window.onload = function() { // will code after page loaded
         .addClass('custom-class')
         .css('text-align', 'center');
 
-    console.log(myElement.html()); /* Output:   <div id="my-element" class="custom-class" style="padding: 150px; margin: 150px; border: 2px solid black; text-align: center;">
+    console.log(myElement.html); /* Output:   <div id="my-element" class="custom-class" style="padding: 150px; margin: 150px; border: 2px solid black; text-align: center;">
                                                     <img src="cat.jpg">
                                                 </div>
                                     */
+    jQueryMini('#my-element2').html('    <img src="cat.jpg"/>');
 }
